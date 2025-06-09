0242b8cc3c0acd3b28164858a6d918e6491d5f46
Yetmgeta <yetmgetae@gmail.com>
2025-06-09 15:38:07 +0300

    update deploy

--- backend/server.ts
+++ backend/server.ts
@@ -10,7 +10,7 @@ dotenv.config();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://sales-csv-processor-frontend.onrender.com"],
   })
 );
 const port = process.env.PORT;
